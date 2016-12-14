eece54786f1fcdd8394400276abd56236895391f
Brionna White <Brionnanwhite@gmail.com>
2016-12-14 06:01:36 -0800

    api

--- api/recipe.ts
+++ api/recipe.ts
@@ -4,7 +4,6 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let request = require('request');
 
-
 //MODEL
 let Recipe = mongoose.model('Recipe', {
   Recipe:{
@@ -14,9 +13,9 @@ let Recipe = mongoose.model('Recipe', {
 
 //save Recipe
 router.post('/recipe', function(req, res) {
-  request('http://food2fork.com/api/search?key=75cf831690ffc9d6e14a46dd5b3c8c13&q='+req.body,
+  request('http://food2fork.com/api/search?key=75cf831690ffc9d6e14a46dd5b3c8c13&q=shredded%20chicken',
     function (error, response, body) {
-      console.log(response)
+      console.log(body)
       let type = JSON.parse(body)
       if (type.name === req.body.type) {
         res.send(type);
