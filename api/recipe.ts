acb4f168b9ef9c3cc481e06a31c8a44a09956590
Brionna White <Brionnanwhite@gmail.com>
2016-12-16 16:32:18 -0800

    make it better

--- api/recipe.ts
+++ api/recipe.ts
@@ -7,7 +7,7 @@ let request = require('request');
 //model
 let Recipe = mongoose.model('Recipe', {
   Recipe:{
-    recipe:String,
+    type:String,
   },
 });
 //food2fork api
@@ -18,9 +18,9 @@ router.post('/recipe', function(req, res) {
   request('http://food2fork.com/api/search?key=75cf831690ffc9d6e14a46dd5b3c8c13&q='+req.body.recipe,
     function (error, response, body) {
       console.log(body)
-      let recipe = JSON.parse(body)
-      if (recipe.name === req.body.recipe) {
-        res.send(recipe);
+      let type = JSON.parse(body)
+      if (type.name === req.body.recipe) {
+        res.send(type);
       } else {
         console.log(error)
         res.send({message:'recipe not found'})
